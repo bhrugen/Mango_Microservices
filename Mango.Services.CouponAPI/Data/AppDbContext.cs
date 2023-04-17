2f85fee55e3cc1361c476bec3efdf70a3b196189
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-17 12:47:31 -0500

    Section 2 - Create AppDbContext

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Mango.Services.CouponAPI/Models/Coupon.cs
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -1,9 +1,14 @@
-﻿namespace Mango.Services.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
+        [Required]
         public string CouponCode { get; set; }
+        [Required]
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
     }
