c3e700283fde37eb8eb102d13e0734ff0c5d0d34
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-17 12:33:32 -0500

    Section 2 - Create Coupon and CouponDTO

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
