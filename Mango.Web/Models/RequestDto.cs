625964ae55be80739cb37793aa7d45b3be11b824
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-19 12:56:45 -0500

    Section 3 - Create Request and Response DTO

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.AccessControl;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
