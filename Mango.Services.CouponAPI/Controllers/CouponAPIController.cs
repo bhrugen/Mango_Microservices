bbe585d1750672c02c14a7691b023e9385cab471
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-19 21:26:03 -0500

    Section 3 - Delete Coupon in Action

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -111,6 +111,7 @@ namespace Mango.Services.CouponAPI.Controllers
         }
 
         [HttpDelete]
+        [Route("{id:int}")]
         public ResponseDto Delete(int id)
         {
             try
