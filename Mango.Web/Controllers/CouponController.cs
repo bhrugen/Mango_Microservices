c7c5ac270fd0c5e566db7765a0399c823453ef14
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-19 21:20:37 -0500

    Section 3 - Delete Coupon and Error

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -48,5 +48,30 @@ namespace Mango.Web.Controllers
             }
             return View(model);
         }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+			ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+
+			if (response != null && response.IsSuccess)
+			{
+				CouponDto? model= JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(model);
+			}
+			return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponsAsync(couponDto.CouponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            return View(couponDto);
+        }
+
     }
 }
