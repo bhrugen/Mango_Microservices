993fe64df8e0e45401e65501b0d37c43351b4a6c
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-19 23:13:09 -0500

    Section 4 - IAuth Service

---
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,35 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+                _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+
+namespace Mango.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
