a6b746f0764e0e3d851cb19a293e279fa2b556a2
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-19 20:54:13 -0500

    Section 3 - API Call in Action

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/coupon")]
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
