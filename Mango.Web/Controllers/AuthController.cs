7412f78bb7ce350c57e24ffbbffc7f575d8e47d2
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-20 13:22:54 -0500

    Section 5 - Auth Controller in Web Project

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+    }
+}
