5de0f541edd247f12543631c53eda76e357423a8
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-20 20:17:58 -0500

    Section 6 - Assignment - Consuming Product API Part 2

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -91,5 +91,38 @@ namespace Mango.Web.Controllers
             return View(productDto);
         }
 
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                ProductDto? model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductEdit(ProductDto productDto)
+        {
+            ResponseDto? response = await _productService.UpdateProductsAsync(productDto);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction(nameof(ProductIndex));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(productDto);
+        }
+
     }
 }
