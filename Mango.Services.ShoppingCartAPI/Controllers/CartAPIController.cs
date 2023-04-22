da608d9db176ea3a3e389504c9f749d5f02a09a6
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-22 13:23:56 -0500

    Section 8 - Remove Cart Details

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -76,5 +76,36 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             return _response;
         }
 
+
+
+        [HttpPost("RemoveCart")]
+        public async Task<ResponseDto> RemoveCart([FromBody]int cartDetailsId)
+        {
+            try
+            {
+                CartDetails cartDetails = _db.CartDetails
+                   .First(u => u.CartDetailsId == cartDetailsId);
+
+                int totalCountofCartItem = _db.CartDetails.Where(u => u.CartHeaderId == cartDetails.CartHeaderId).Count();
+                _db.CartDetails.Remove(cartDetails);
+                if (totalCountofCartItem == 1)
+                {
+                    var cartHeaderToRemove = await _db.CartHeaders
+                       .FirstOrDefaultAsync(u => u.CartHeaderId == cartDetails.CartHeaderId);
+
+                    _db.CartHeaders.Remove(cartHeaderToRemove);
+                }
+                await _db.SaveChangesAsync();
+               
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message.ToString();
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+
     }
 }
