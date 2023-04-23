6437edc1e5732045256542655da302dc31dbe7d4
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-23 14:31:07 -0500

    Section 11 - Implement Processor for Service Bus

---
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace Mango.Services.EmailAPI.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionString;
+        private readonly string emailCartQueue;
+        private readonly IConfiguration _configuration;
+
+        private ServiceBusProcessor _emailCartProcessor;
+
+        public AzureServiceBusConsumer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+
+            emailCartQueue = _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+
+            var client = new ServiceBusClient(serviceBusConnectionString);
+            _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+        }
+
+
+    }
+}
--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -99,7 +99,7 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         {
             try
             {
-                await _messageBus.PublishMessage(cartDto, _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCart"));
+                await _messageBus.PublishMessage(cartDto, _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue"));
                 _response.Result = true;
             }
             catch (Exception ex)
