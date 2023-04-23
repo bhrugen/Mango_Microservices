3d3997d62ea9317cfee97a4b41c3cb8a2c63b69d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-23 13:30:19 -0500

    Section 10 - MessageBus Interface

---
+++ Mango.MessageBus/IMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public interface IMessageBus
+    {
+        Task PublishMessage(object message, string topic_queue_Name);
+    }
+}
