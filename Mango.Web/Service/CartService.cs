903b335b2f525ce841b10fc6570b7d7907b8d884
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-22 21:34:04 -0500

    Section 9 - Fixing a bug

--- Mango.Web/Service/CartService.cs
+++ Mango.Web/Service/CartService.cs
@@ -27,7 +27,7 @@ namespace Mango.Web.Service
             return await _baseService.SendAsync(new RequestDto()
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.ShoppingCartAPIBase + "/api/coupon/GetCart/"+ userId
+                Url = SD.ShoppingCartAPIBase + "/api/cart/GetCart/"+ userId
             });
         }
 
