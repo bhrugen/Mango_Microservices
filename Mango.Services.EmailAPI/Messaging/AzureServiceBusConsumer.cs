30fde72cac69538d345fd224656823c4d97b47a3
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-23 14:39:11 -0500

    Section 11 - Register methods to Processor

--- Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,8 +1,11 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.EmailAPI.Models.Dto;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Mango.Services.EmailAPI.Messaging
 {
-    public class AzureServiceBusConsumer
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
     {
         private readonly string serviceBusConnectionString;
         private readonly string emailCartQueue;
@@ -22,6 +25,42 @@ namespace Mango.Services.EmailAPI.Messaging
             _emailCartProcessor = client.CreateProcessor(emailCartQueue);
         }
 
+        public async Task Start()
+        {
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+        }
 
+        public async Task Stop()
+        {
+            await _emailCartProcessor.StopProcessingAsync();
+            await _emailCartProcessor.DisposeAsync();
+        }
+
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            //this is where you will receive message
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CartDto objMessage = JsonConvert.DeserializeObject<CartDto>(body);
+            try
+            {
+                //TODO - try to log email
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception ex) {
+                throw;
+            }
+
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+       
     }
 }
---
+++ Mango.Services.EmailAPI/Messaging/IAzureServiceBusConsumer.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.EmailAPI.Messaging
+{
+    public interface IAzureServiceBusConsumer
+    {
+        Task Start();
+        Task Stop();
+    }
+}
