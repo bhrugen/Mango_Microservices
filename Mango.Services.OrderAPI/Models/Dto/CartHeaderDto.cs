630cdd73d65d9ddfe139eebe81be4868f03b6ccc
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-24 12:48:44 -0500

    Section 12 - Add DTO's in Order API

---
+++ Mango.Services.OrderAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.OrderAPI.Models.Dto
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+       
+       
+        public string? Name { get; set; }
+        
+        public string? Phone { get; set; }
+   
+        public string? Email { get; set; }
+    }
+}
