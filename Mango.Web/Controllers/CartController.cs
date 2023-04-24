09cfed036d3cc03d202df062e010c4cbf12d8f44
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-24 12:18:56 -0500

    Section 12 - Checkout UI

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -21,6 +21,12 @@ namespace Mango.Web.Controllers
             return View(await LoadCartDtoBasedOnLoggedInUser());
         }
 
+        [Authorize]
+        public async Task<IActionResult> Checkout()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
         public async Task<IActionResult> Remove(int cartDetailsId)
         {
             var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
