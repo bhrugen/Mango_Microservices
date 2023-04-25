e876bb38921c746bdbf88b983bc5ff15e7ab235c
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-25 12:39:02 -0500

    Section 13 - Stripe Flow and Stripe DTO

---
+++ Mango.Web/Models/StripeRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Web.Models
+{
+    public class StripeRequestDto
+    {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSessionId { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDto OrderHeader { get; set; }
+    }
+}
