35a5b173c3adcfca758ce5f9380ee56248d845c1
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-26 13:48:13 -0500

    Section 15 - Order List UI

---
+++ Mango.Web/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult OrderIndex()
+        {
+            return View();
+        }
+    }
+}
