c5c1c0dc325dbf471c334db7c510f1cc27efe9e2
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-26 13:59:23 -0500

    Section 15 - Configure Datatable Endpoint

--- Mango.Web/Controllers/OrderController.cs
+++ Mango.Web/Controllers/OrderController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Mango.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
         public IActionResult OrderIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            IEnumerable<OrderHeaderDto> list;
+            string userId = "";
+            if (!User.IsInRole(SD.RoleAdmin))
+            {
+                userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+            ResponseDto response = _orderService.GetAllOrder(userId).GetAwaiter().GetResult();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDto>();
+            }
+            return Json(new { data = list });
         }
     }
 }
---
+++ Mango.Web/wwwroot/js/order.js
@@ -0,0 +1,11 @@
+﻿var dataTable;
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: "/order/getall" },
+        "columns": [
+            { data: 'orderheaderid', "width": "5%"},
+            { data: 'email', "width": "25%"}
+        ]
+    })
+}
