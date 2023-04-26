66bc24a2c3585522d4264ca8c276b9e6015d215f
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-26 14:18:09 -0500

    Section 15 - Load Datatables

--- Mango.Web/wwwroot/js/order.js
+++ Mango.Web/wwwroot/js/order.js
@@ -1,11 +1,28 @@
 ﻿var dataTable;
 
+$(document).ready(function () {
+    loadDataTable();
+});
+
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: "/order/getall" },
         "columns": [
-            { data: 'orderheaderid', "width": "5%"},
-            { data: 'email', "width": "25%"}
+            { data: 'orderHeaderId', "width": "5%"},
+            { data: 'email', "width": "25%" },
+            { data: 'name', "width": "20%" },
+            { data: 'phone', "width": "10%" },
+            { data: 'status', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'orderHeaderId',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/order/orderDetail?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
+                    </div>`
+                },
+                "width": "10%"
+            }
         ]
     })
 }
