3ec31244cf331920fd4833b7e5b37cac69b09539
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-28 13:29:14 -0500

    Section 16 - Post Endpoint in ProductAPI

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -61,11 +61,30 @@ namespace Mango.Services.ProductAPI.Controllers
         {
             try
             {
-                Product obj = _mapper.Map<Product>(ProductDto);
-                _db.Products.Add(obj);
+                Product product = _mapper.Map<Product>(ProductDto);
+                _db.Products.Add(product);
                 _db.SaveChanges();
 
-                _response.Result = _mapper.Map<ProductDto>(obj);
+                if (ProductDto.Image != null)
+                {
+                    string fileName = product.ProductId + Path.GetExtension(ProductDto.Image.FileName);
+                    string filePath = @"wwwroot\ProductImages\" + fileName;
+                    var filePathDirectory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+                    using (var fileStream = new FileStream(filePathDirectory, FileMode.Create))
+                    {
+                        ProductDto.Image.CopyTo(fileStream);
+                    }
+                    var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
+                    product.ImageUrl = baseUrl+ "/ProductImages/"+ filePath;
+                    product.ImageLocalPath = filePath;
+                }
+                else
+                {
+                    product.ImageUrl = "https://placehold.co/600x400";
+                }
+                _db.Products.Update(product);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<ProductDto>(product);
             }
             catch (Exception ex)
             {
