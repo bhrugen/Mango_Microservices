50be5b2b13b7c3ae6dabeded815f9bd589770671
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-29 14:37:18 -0500

    Section 19 -  Create Rabbmit MQ Consumer Part 2

--- Mango.Services.EmailAPI/Messaging/RabbitMQAuthConsumer.cs
+++ Mango.Services.EmailAPI/Messaging/RabbitMQAuthConsumer.cs
@@ -1,6 +1,58 @@
-﻿namespace Mango.Services.EmailAPI.Messaging
+﻿using Mango.Services.EmailAPI.Services;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Mango.Services.EmailAPI.Messaging
 {
-    public class RabbitMQAuthConsumer
+    public class RabbitMQAuthConsumer : BackgroundService
     {
+        private readonly IConfiguration _configuration;
+        private readonly EmailService _emailService;
+        private IConnection _connection;
+        private IModel _channel;
+        public RabbitMQAuthConsumer(IConfiguration configuration, EmailService emailService)
+        {
+            _configuration = configuration;
+            _emailService = emailService;
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Password = "guest",
+                UserName = "guest",
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(_configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue")
+                , false, false, false, null);
+
+        }
+
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                String email = JsonConvert.DeserializeObject<string>(content);
+                HandleMessage(email).GetAwaiter().GetResult();
+
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(_configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue"), false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleMessage(string email)
+        {
+
+        }
     }
 }
