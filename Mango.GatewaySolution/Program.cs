dcdafbbf3a3116392b956a13b36380151997ed4b
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-29 12:18:10 -0500

    Section 17 - Clean Code

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -5,11 +5,12 @@ using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthetication();
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
