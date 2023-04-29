863ea6f4b879d07a5467a519a2af527f33176d38
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-29 11:19:08 -0500

    Section 17 - Product functional with Ocelot

--- Mango.Web/Service/IService/IProductService.cs
+++ Mango.Web/Service/IService/IProductService.cs
@@ -4,7 +4,7 @@ namespace Mango.Web.Service.IService
 {
     public interface IProductService
     {
-        Task<ResponseDto?> GetProductAsync(string couponCode);
+     
         Task<ResponseDto?> GetAllProductsAsync();
         Task<ResponseDto?> GetProductByIdAsync(int id);
         Task<ResponseDto?> CreateProductsAsync(ProductDto productDto);
--- Mango.Web/Service/ProductService.cs
+++ Mango.Web/Service/ProductService.cs
@@ -41,14 +41,7 @@ namespace Mango.Web.Service
             });
         }
 
-        public async Task<ResponseDto?> GetProductAsync(string productCode)
-        {
-            return await _baseService.SendAsync(new RequestDto()
-            {
-                ApiType = SD.ApiType.GET,
-                Url = SD.ProductAPIBase + "/api/product/GetByCode/"+productCode
-            });
-        }
+      
 
         public async Task<ResponseDto?> GetProductByIdAsync(int id)
         {
