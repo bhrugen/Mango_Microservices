a208e8b63f419c9f1dab1a28b551123c2e768dab
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-02 08:07:28 -0500

    Section 20 - Ocelot Deployed to Production

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -5,7 +5,14 @@ using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthetication();
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
+{
+    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+}
 builder.Services.AddOcelot(builder.Configuration);
 
 
